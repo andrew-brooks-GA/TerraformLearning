754c81fe2e407fe13fd2222ff8a3d738f2287802
abrooks90 <andrew.brooks.aob@gmail.com>
2024-04-28 09:03:57 -0400

    Creates a shared volume to persist data for the valheim game server.

---
+++ terraform-docker-shared_volume/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_volume" "shared_volume" {
+  name = "shared_volume"
+}
